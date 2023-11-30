b867909daa8d4c16df1de438eb1ed1dda2ed3a6f
estrela1921 <143469066+estrela1921@users.noreply.github.com>
2023-11-29 22:17:37 -0300

    Update resiliadata.ind.sql

--- resiliadata.ind.sql
+++ resiliadata.ind.sql
@@ -1,24 +1,24 @@
-use resiliadata;
+create database resiliadata;
 
 create table empresas_parceiras (
-    id_empresa int not null primary key auto_increment,
-    nome varchar(100),
-    endereco varchar(150)
+    id_empresa int  primary key auto_increment,
+    nome varchar(100) not null,
+    endereco varchar(150) not null
 );
 
 create table areas (
-    id int not null primary key auto_increment,
-    nome varchar(100)
+    id_area int  primary key auto_increment,
+    nome varchar(100) not null
 );
 
 create table tecnologias (
-    id_tecnologia int not null primary key auto_increment,
-    nome varchar(100)
+    id_tecnologia int  primary key auto_increment,
+    nome varchar(100) not null
     );    
    
 create table colaboradores (
     id_colaborador int not null primary key auto_increment,
-    nome  varchar(100)
+    nome  varchar(100) not null
     );    
    
 create table tecnologias_empresas (
@@ -51,8 +51,20 @@ alter table tecnologias_empresas
 add constraint fk_tecnologias_empresas_id_tecnologia
 foreign key (id_tecnologia) references tecnologias(id_tecnologia);
 
+INSERT INTO empresas_parceiras(id_empresa, nome, endereco) VALUES (1 ,'Tecnoforma','Rua do rio,1200');
+INSERT INTO empresas_parceiras(id_empresa, nome, endereco) VALUES (2 ,'Tecdata','Rua Margarida,100');
 
- 
+INSERT INTO areas(id_area, nome) VALUES (1 ,'webdev');
+INSERT INTO areas(id_area, nome) VALUES (2 ,'dados');
+
+INSERT INTO tecnologias(id_tecnologia, nome, id_area) VALUES (1 ,' JavaScript', 1);
+INSERT INTO tecnologias(id_tecnologia, nome, id_area) VALUES (2 ,'MySQL', 2);
+
+INSERT INTO colaboradores(id_colaborador, nome, id_empresa) VALUES (1 ,'Andre Moraes', 1);
+INSERT INTO colaboradores(id_colaborador, nome, id_empresa) VALUES (2 ,'Angela Silva', 2);
+
+INSERT INTO tecnologias_empresas(id_empresa, id_tecnologia) VALUES (1 , 1);
+INSERT INTO tecnologias_empresas(id_empresa, id_tecnologia) VALUES (2 , 2);
 
 
 
